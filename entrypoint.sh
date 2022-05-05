038632fce8bd253f1d772f9f31a89cdcaed6c77c
Girish Joshi <girish946@gmail.com>
2022-05-05 17:31:38 +0530

    added file exists check to the entrypoint
    Signed-off-by: Girish Joshi <girish946@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
 
+echo "Hello $1"
+file_exists=0
+
+if test -f "$2"; then
+    echo "$2 exists."
+    file_exists=1
+else
+    exit 1
+fi
+
+file_contents=`cat $2`
+
+echo "::set-output name=file_exists::$file_exists"
+echo "::set-output name=file_contents::$file_contents"
